66a0989b462175d544d87f847a49bf1a17eb304f
MagiShi <maggie@lawn-128-61-39-111.lawn.gatech.edu>
2017-04-19 19:24:24 -0400

    Update to firebase now works

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,18 @@
+
+
+
 var reference = firebase.database();
 var itemNameField = document.getElementById('itemName');
 var barcodeField = document.getElementById('barcode');
 var descField = document.getElementById('shortDescription');
+var number = 1;
 
 function saveAddItemData() {
     var item = itemNameField.value;
     var id = barcodeField.value;
     var description = descField.value;
 
-    reference.ref('items/').update(
+    reference.ref('items/' + number++).update(
         {name:item,
             desc:description,
             barcode:id}
