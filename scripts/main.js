7ac0b7d4e774434e0346460ebfd39a318b383ba7
dcochs3 <dcochs3@gmail.com>
2017-04-19 19:10:55 -0400

    Add files via upload
    Initial js file for database interaction and moving forward.

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+var reference = firebase.database();
+var itemNameField = document.getElementById('itemName');
+var barcodeField = document.getElementById('barcode');
+var descField = document.getElementById('shortDescription');
+
+function saveAddItemData() {
+    var item = itemNameField.value;
+    var id = barcodeField.value;
+    var description = descField.value;
+
+    reference.ref('items/').update(
+        {name:item,
+            desc:description,
+            barcode:id}
+    );
+}
