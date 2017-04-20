be16f3964afbd45f5a5d2177252d1f9723f8b16f
MagiShi <maggie@lawn-128-61-39-111.lawn.gatech.edu>
2017-04-19 21:21:22 -0400

    Fixed bug with adding to firebase

--- scripts/main.js
+++ scripts/main.js
@@ -5,14 +5,14 @@ var reference = firebase.database();
 var itemNameField = document.getElementById('itemName');
 var barcodeField = document.getElementById('barcode');
 var descField = document.getElementById('shortDescription');
-var number = 1;
+var number = 7;
 
 function saveAddItemData() {
     var item = itemNameField.value;
     var id = barcodeField.value;
     var description = descField.value;
 
-    reference.ref('items/' + number++).update(
+    reference.ref('items/' + item).update(
         {name:item,
             desc:description,
             barcode:id}
