a43aadd38c341b181be996feb314f231f60c739e
Phuc Huynh <phuchuynh@gatech.edu>
2018-01-24 17:39:31 -0500

    another testing

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     #This is a test
-    return "1Hello World! This should be in the master branch"
-    return "Phuc's testing"
+    return "1Hello World! This should be in the master branch. Another testing"
 
 
