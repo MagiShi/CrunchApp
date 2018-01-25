7c4cc9af94860d68ec1a966a4df6006340b5dc42
Dana Ochs <dcochs3@gmail.com>
2018-01-24 21:09:12 -0500

    added linking for returning user, use as guest, and register buttons

--- app.py
+++ app.py
@@ -6,13 +6,22 @@ app = Flask(__name__)
 def home():
     return render_template('login.html')
 
+@app.route('/login.html')
+def returningUser():
+    return render_template('login.html')
+
 @app.route('/home.html')
 def login():
     return render_template('home.html')
+def guest():
+    # should look different than a registered user (not able to add, delete, etc)
+    return render_template('home.html')
 
 @app.route('/register.html')
-def register():
+def newUser():
     return render_template('register.html')
+def register():
+    return render_template('home.html')
 
 if __name__ == "__main__":
     app.run()
