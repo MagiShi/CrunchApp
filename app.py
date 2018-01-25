a38b9ea2328e9734f10d23783a3167c8a4c59fd3
Maggie Shi <maggie@lawn-128-61-45-119.lawn.gatech.edu>
2018-01-25 18:33:15 -0500

    testing database connection

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask, flash, redirect, render_template, request, session, abort
  
+from flask.ext.sqlalchemy import SQLAlchemy
+import os
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
+
+# app = Flask(__name__)
  
 @app.route('/')
 def home():
