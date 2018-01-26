c066afc8348f1c8b753fe74688e50d14d08d9da1
Maggie Shi <maggieqshi@gmail.com>
2018-01-26 07:32:53 -0500

    #9 Registration inserts into the database.  Change some code in register.html regarding form and btn types

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import psycopg2
 
 app = Flask(__name__)
 
-url = urlparse(os.environ.get('DATABASE_URL'))
+url = urlparse("postgres://mbgugkwmyyrgpp:08f1f171ba8df81468de5e7d166069757cc545fb163d5cc820407068513b101d@ec2-54-163-237-249.compute-1.amazonaws.com:5432/da0io40vrbg6u0")
 db = "dbname=%s user=%s password=%s host=%s " % (url.path[1:], url.username, url.password, url.hostname)
 schema = "schema.sql"
 conn = psycopg2.connect(db)
@@ -22,9 +22,9 @@ def login():
     username = request.form['username']
     password = request.form['password']
 
-    sql = "SELECT * FROM registereduser WHERE username = '{0}' AND password = '{1}';".format(username, password)
-    print (sql)
-    cursor.execute(sql)
+    query = "SELECT * FROM registereduser WHERE username = '{0}' AND password = '{1}';".format(username, password)
+    print (query)
+    cursor.execute(query)
     # print (cursor.fetchone())
     tup = cursor.fetchone()
 
@@ -44,17 +44,29 @@ def guest():
 def newUser():
     return render_template('register.html')
 
-@app.route('/register.html', methods=['POST'])
+@app.route('/postregister.html', methods=['POST'])
 def register():
+    # print ("in here")
     username = request.form['username']
     email = request.form['email']
     password = request.form['password']
+
+    #Account type not accessed
     accountType = request.form['accountType']
     if accountType == 'studentAccount':
         isAdmin = False
     else:
         isAdmin = True
-    return render_template('home.html')
+
+    query = "INSERT into registereduser values ('{0}', '{1}', '{2}', {3});".format(email, username, password, isAdmin)
+    print (query)
+    # print (cursor.execute(query))
+    # print (cursor.fetchone())
+    cursor.execute(query)
+    # tup = cursor.fetchone()
+    # print (tup)
+
+    return render_template('login.html')
 
 @app.route('/addItem.html')
 def add():
