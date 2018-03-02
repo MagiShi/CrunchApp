28caa801698a4c5c2e8614950b9ac3949a3a9662
dainryoo <dainryoo@gmail.com>
2018-03-02 11:37:43 -0500

    #74 All filter characteristics under the filter menu

--- static/navbar.js
+++ static/navbar.js
@@ -1,3 +1,4 @@
+// For the opening and closing of the left filter menu
 function toggleFilterMenu() {
     if (document.getElementById("filter-menu").style.width == "300px") {
         var menuItems = document.getElementById("filter-menu").getElementsByTagName('a');
@@ -18,7 +19,17 @@ function toggleFilterMenu() {
     }
 }
 
+// For the opening and closing of the "accordion panels" within the filter menu
+function toggleAccordion(filterButton) {
+    var panel = filterButton.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+}
 
+// For the opening and closing of the right account menu
 function toggleAccountMenu() {
     if (document.getElementById("account-menu").style.width == "250px") {
         var menuItems = document.getElementById("account-menu").getElementsByTagName('a');
