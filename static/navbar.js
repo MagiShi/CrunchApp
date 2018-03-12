50d8923e825c166653370293c5cb3cfdec3cab43
dainryoo <dainryoo@gmail.com>
2018-03-12 10:29:19 -0400

    Fixed the two menu open/close toggles

--- static/navbar.js
+++ static/navbar.js
@@ -31,14 +31,14 @@ function toggleAccordion(filterButton) {
 
 // For the opening and closing of the right account menu
 function toggleAccountMenu() {
-    if (document.getElementById("account-menu").style.width == "250px") {
+    if (document.getElementById("account-menu").style.width == "300px") {
         var menuItems = document.getElementById("account-menu").getElementsByTagName('a');
         for (var i = 1; i < menuItems.length; i++) {
             menuItems[i].style.opacity = 0;
         }
         document.getElementById("account-menu").style.width = "0";
     } else {
-        if (document.getElementById("filter-menu").style.width == "250px") {
+        if (document.getElementById("filter-menu").style.width == "300px") {
             toggleFilterMenu();
         }
 
@@ -46,6 +46,6 @@ function toggleAccountMenu() {
         for (var i = 1; i < menuItems.length; i++) {
             menuItems[i].style.opacity = 1;
         }
-        document.getElementById("account-menu").style.width = "250px";
+        document.getElementById("account-menu").style.width = "300px";
     }
 }
