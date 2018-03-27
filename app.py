ee4b44a1ebb4ffc1d3dddc5145e403e0c1d995fe
Tommy <tristau3@gatech.edu>
2018-03-27 17:35:42 -0400

    working on #46

--- app.py
+++ app.py
@@ -791,7 +791,7 @@ def filterItems():
     try: 
         cursor.execute(query)
         #conn.commit()
-        itemname = cursor.fetchall()
+        itemid = cursor.fetchall()
         error = 'Items filtered (temp message)'
     except Exception as e: 
         cursor.execute("rollback;")
@@ -799,7 +799,7 @@ def filterItems():
         ##Error
         error = 'Cannot filter'
         return redirect(url_for('loggedin', error=error))
-    return render_template('home.html', itemname=itemname, error=error)
+    return render_template('home.html', itemname=itemid, error=error)
 
 #Adding a new folder 
 @app.route('/addFolder', methods=["POST"])
