f0ac2b0dbe45e800da8aa8b5e150888bfa633d35
tristau62 <tristau62@gmail.com>
2018-04-13 11:56:54 -0400

    reverting changes

--- app.py
+++ app.py
@@ -101,12 +101,11 @@ def loggedin():
     cursor.execute(itemNameQuery)
     itemname = cursor.fetchall()
     cursor.execute(imageQuery)
-    image = cursor.fetchall()
     for x in image:
         imagedata = []
         if x[0] != None:
             imagedata = functions.getImagedata(x)
-            x[0] = imagedata
+            x = imagedata
 
     return render_template('home.html', itemid=itemid, itemname=itemname, image=image)
 
